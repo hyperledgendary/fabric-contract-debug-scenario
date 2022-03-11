2e486b3ecd554e4bd91abb46b29738b85bedd3d7
Matthew B White <whitemat@uk.ibm.com>
2022-03-11 16:46:19 +0000

    Updates
    Signed-off-by: Matthew B White <whitemat@uk.ibm.com>

--- infrastructure/dev-microfab/oneorg.sh
+++ infrastructure/dev-microfab/oneorg.sh
@@ -15,6 +15,6 @@ export MICROFAB_CONFIG='{
     "capability_level":"V2_0"
 }'
 
-LOGGING_SPEC=ccprovider=debug:lifecycle=debug:info
-docker run --name microfab --rm -ti -p2005:2005 -p 8080:8080  -e MICROFAB_CONFIG="${MICROFAB_CONFIG}" -e FABRIC_LOGGING_SPEC=$LOGGING_SPEC ibmcom/ibp-microfab
+LOGGING_SPEC=debug
+docker run --name microfab --rm -ti -p2005:2005 -p 8080:8080  -e MICROFAB_CONFIG="${MICROFAB_CONFIG}" -e FABRIC_LOGGING_SPEC=$LOGGING_SPEC ibmcom/ibp-microfab-rc
 
